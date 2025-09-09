f106e882169025f0f4aaeca56014ba97be50625a
bp-riv <165778361+bp-riv@users.noreply.github.com>
2025-09-09 14:54:15 +0200

    Notebook Check Prototype: V2

--- doc/source/samples/run_notebook_samples.py
+++ doc/source/samples/run_notebook_samples.py
@@ -1,7 +1,6 @@
 # Check if all the notebooks collected in the documentation run successfully
 import sys
 import os
-import unittest
 import warnings
 from pathlib import Path
 import nbformat
@@ -10,8 +9,9 @@ warnings.filterwarnings("ignore")
 
 
 def execute_notebook(notebook_file: Path, 
-                        notebook_folder: Path,
-                            timeout: int = 600) -> None:
+                     notebook_folder: Path,
+                     kernel_name = "eao_env",
+                     timeout: int = 600) -> None:
 
     """ Simple function to RUN a Jupyter notebook.
     If all the cells are executed, returns True, otherwise False.
@@ -26,9 +26,9 @@ def execute_notebook(notebook_file: Path,
             nb = nbformat.read(f, as_version=4)
 
         # Configure and execute the notebook
-        ep = ExecutePreprocessor(timeout=timeout)
+        ep = ExecutePreprocessor(timeout=timeout,
+                                 kernel_name = kernel_name)
         # Run notebooks as if they were in their original folder
-#       ep.preprocess(nb,{'metadata': {'path': './docs/source/notebooks'}})
         ep.preprocess(nb,{'metadata': {'path': notebook_folder}})
 
     except FileNotFoundError:
@@ -43,22 +43,76 @@ def execute_notebook(notebook_file: Path,
         print(msg)
         return False
 
-    print("OK")
+    print(f"Notebook {notebook_path}: SUCCESS")
     return True
     #---
+
+
+def find_all_notebooks():
+    '''
+    Visit the folder doc/source/samples and identifies all the
+    existing notebooks together with their folders.
+    Retuns the list of pairs (folder, notebook_file).
+    This is important since all the notebooks should be run as
+    if they where in their original folder, therefore the tracking.
+    '''
+    # Assume to work in the following path
+    path_all_notebooks = os.path.join("doc", "source", "samples")
+    all_folders = os.listdir(path_all_notebooks)
+    # In each directory, seach for notebooks, store (folder,file)
+    pairs = []
+    for folder in all_folders:
+        path_folder = os.path.join(path_all_notebooks, folder)
+        # if it is a folder, then check notebooks
+        if os.path.isdir(path_folder):
+            print(f"Checking folder {path_folder}")
+            # Check if there are files ending with ipynb
+            for file in os.listdir(path_folder):
+                if len(str(file)) > 6:
+                    if str(file)[-6:] == ".ipynb":
+                        print(f"\t{file}")
+                        print(f"{path_folder}, {file}")
+                        # Append to the pair list
+                        pairs.append((path_folder, file))
+        else:
+            print(f"File {path_folder} not a folder: SKIP")
+    print(f"Pairs found:")
+    for p in pairs:
+        print(f"{p[0]} + {p[1]}")
+    print(f"Total of {len(pairs)} notebooks to check.")
+    return pairs
+
 
 def test_notebook_execution():
     """ Run all the notebooks and return True if there is no error at all.
     """
     # Use the previous function to run every notebook of interest
-    notebook_folder = os.path.join("empty_notebook")
-    my_notebooks = ["import_eao.ipynb"]
-    for name in my_notebooks:
-        print(f"Testing {my_notebooks}")
-        res = execute_notebook(name, notebook_folder)
+    folder_file_pairs = find_all_notebooks()
+    tot = len(folder_file_pairs)
+    print(f"Total of {tot} notebooks to run.")
+    fail = []
+    # Use the kernel with the same name of conda environment
+    my_kernel = os.getenv("CONDA_DEFAULT_ENV")
+    if not my_kernel:
+        print("Unable to load a kernel! Sure that you use conda?")
+        return False
+
+    for path_folder, filename in folder_file_pairs:
+        full_file =os.path.join(path_folder, filename)
+        print(f"Testing: {full_file}")
+        input("OK? (TO BE REMOVED)")
+        res = execute_notebook(filename, path_folder, my_kernel)
         if res is False:
-            return False
+            print(f"Notebook: {full_file}: FAIL")
+            fail.append(full_file)
+
+    if len(fail) > 0:
+        print("The following notebooks FAILED:")
+        for fullfile in fail:
+            print(fullfile)
+        return False
     return True
+#---
 
 if __name__ == "__main__":
     res = test_notebook_execution()
