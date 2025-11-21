9e6b34a27396253427b04c896ee9057bf3ac2729
bp-riv <165778361+bp-riv@users.noreply.github.com>
2025-11-21 10:11:18 +0100

    Notebook badge: checking

--- doc/source/samples/run_notebook_samples.py
+++ doc/source/samples/run_notebook_samples.py
@@ -119,11 +119,12 @@ def test_notebook_execution():
 #---
 
 if __name__ == "__main__":
+    # DEBUG: FORCING A FAIL TO CHECK RED MARK. REMOVE THIS AFTERWARDS
+    sys.exit(1) # <- REMOVE THIS
+    #------------------------------------------------------#
     folder_file_pairs = find_all_notebooks()
     res = test_notebook_execution()
     print(res)
-    # DEBUG: FORCING A FAIL TO CHECK RED MARK. REMOVE THIS AFTERWARDS
-    sys.exit(1) # <- REMOVE THIS
     if res:
         print("Running Samples: SUCCESS")
         sys.exit(0)
